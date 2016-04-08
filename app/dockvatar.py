d22da07d41be015bf0dbfe64c0ba5381557d194d
aieeeeeeeee <benoit.voirin@gmail.com>
2016-04-08 11:35:31 +0200

    Test simple change in staging

--- app/dockvatar.py
+++ app/dockvatar.py
@@ -27,7 +27,7 @@ def mainpage():
               <p>You like a:
               <img src="/monster/{1}" />
               '''.format(name, name_hash)
-    footer = '<p>Footer from staging</body></html>'
+    footer = '<p>Footer OK</body></html>'
 
     return header + body + footer
 
