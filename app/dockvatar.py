efc586a2878ec9b9dcffc8d0c00c1a2526d9f78c
aieeeeeeeee <benoit.voirin@gmail.com>
2016-04-27 10:48:43 +0200

    Added comment to trigger a build on staging

--- app/dockvatar.py
+++ app/dockvatar.py
@@ -6,6 +6,7 @@ import html
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
+# Unique salt to make sure nobody else will generate the same image
 salt = "AIEEEEEEEEE"
 default_name = 'Dockvatar'
 
